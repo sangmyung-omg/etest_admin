c990a906a2891a107bca548b284cfcac3be4ae0b
이진형 <jinhyung_lee@tmax.co.kr>
2021-11-17 15:39:15 +0900

    [feat] uk version 추가에 따라 UKList가 최신 version의 uk 정보들을 가져오도록 수정

--- eTest/src/main/java/com/tmax/eTest/Common/repository/uk/UkDescriptionVersionRepo.java
+++ eTest/src/main/java/com/tmax/eTest/Common/repository/uk/UkDescriptionVersionRepo.java
@@ -3,8 +3,10 @@ package com.tmax.eTest.Common.repository.uk;
 import com.tmax.eTest.Common.model.uk.UkDescriptionVersion;
 import com.tmax.eTest.Common.model.uk.UkDesriptionVersionCompositeKey;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UkDescriptionVersionRepo extends JpaRepository<UkDescriptionVersion, UkDesriptionVersionCompositeKey> {
-    
+    List<UkDescriptionVersion> findByVersionIdOrderByUkId(Long versionId);
 }
