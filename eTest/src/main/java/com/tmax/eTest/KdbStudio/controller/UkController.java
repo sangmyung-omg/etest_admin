2c2408a0236cdc771d07c9c993268292c3076bee
이상명 <sangmyung_lee@tmax.co.kr>
2021-11-22 10:01:05 +0000

    feat: Add UK versioning related APIs to kdbstudio directory

--- eTest/src/main/java/com/tmax/eTest/KdbStudio/controller/UkController.java
+++ eTest/src/main/java/com/tmax/eTest/KdbStudio/controller/UkController.java
@@ -1,6 +1,8 @@
 package com.tmax.eTest.KdbStudio.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.tmax.eTest.Common.model.uk.UkDescriptionVersion;
 import com.tmax.eTest.KdbStudio.dto.UkUpdateDTO;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -42,6 +45,16 @@ public class UkController {
     public ResponseEntity<Object> updateUkInfo(@RequestParam Integer ukId, @RequestParam Integer versionId,
                                                     @RequestBody UkUpdateDTO inputBody) {
         log.info("> Update Uk Info. ukId : " + Integer.toString(ukId) + ", versionId : " + Integer.toString(versionId) + ", inputBody : " + inputBody.toString());
-        return new ResponseEntity<>(HttpStatus.OK);
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        try {
+            String message = ukService.updateUkInfo(new Long(ukId), new Long(versionId), inputBody);
+            map.put("message", message);
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            log.info("error : No data found for the version id = " + Long.toString(versionId) + " and uk id = " + Long.toString(ukId));
+            map.put("error", "error : No data found for the version id = " + Long.toString(versionId) + " and uk id = " + Long.toString(ukId));
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
     }
 }
--- eTest/src/main/java/com/tmax/eTest/KdbStudio/service/UkService.java
+++ eTest/src/main/java/com/tmax/eTest/KdbStudio/service/UkService.java
@@ -3,10 +3,14 @@ package com.tmax.eTest.KdbStudio.service;
 import java.util.List;
 
 import com.tmax.eTest.Common.model.uk.UkDescriptionVersion;
+import com.tmax.eTest.Common.model.uk.UkDesriptionVersionCompositeKey;
 import com.tmax.eTest.Common.repository.uk.UkDescriptionVersionRepo;
+import com.tmax.eTest.KdbStudio.dto.UkUpdateDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javassist.NotFoundException;
 
 @Service
 public class UkService {
@@ -17,5 +21,21 @@ public class UkService {
     public List<UkDescriptionVersion> getAllUkInfoForVersion(Integer versionId) {
         List<UkDescriptionVersion> ukList = ukVersionRepo.findByVersionIdOrderByUkId(new Long(versionId));
         return ukList;
+    }
+
+    public String updateUkInfo(Long ukId, Long versionId, UkUpdateDTO input) throws NotFoundException {
+        // get existing uk info
+        UkDesriptionVersionCompositeKey keyset = new UkDesriptionVersionCompositeKey();
+        keyset.setUkId(ukId);
+        keyset.setVersionId(versionId);
+        UkDescriptionVersion ukInfo = ukVersionRepo.findById(keyset).orElseThrow(() -> new NotFoundException("error : The version id given is not found, " + Long.toString(versionId)));
+
+        // update uk info
+        ukInfo.setUkName(input.getUkName());
+        if (input.getUkDescription() != null && !input.getUkDescription().equalsIgnoreCase(""))
+            ukInfo.setUkDescription(input.getUkDescription());
+        ukVersionRepo.save(ukInfo);
+
+        return "Successfully updated Uk info.";
     }
 }
