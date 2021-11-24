6baed0b549c86902e062ce6e5642547ca8fa75f0
송영근 <younggeun_song@tmax.co.kr>
2021-11-24 07:33:41 +0000

    Ae1 2/feature/contents

---
+++ eTest/src/main/java/com/tmax/eTest/Contents/dto/SizeDTO.java
@@ -0,0 +1,12 @@
+package com.tmax.eTest.Contents.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SizeDTO {
+  Long size;
+}
