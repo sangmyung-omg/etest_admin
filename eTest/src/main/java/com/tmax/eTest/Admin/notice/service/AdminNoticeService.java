dc38ccb170b3019d7f30d66f0946672818f6f633
정용조 <yongjo_jeong@tmax.co.kr>
2021-11-30 16:26:27 +0900

    [feat] send push when new notice is added

--- eTest/src/main/java/com/tmax/eTest/Admin/notice/service/AdminNoticeService.java
+++ eTest/src/main/java/com/tmax/eTest/Admin/notice/service/AdminNoticeService.java
@@ -4,6 +4,8 @@ import com.tmax.eTest.Admin.notice.repository.AdminNoticeRepository;
 import com.tmax.eTest.Admin.notice.repository.AdminNoticeRepositorySupport;
 import com.tmax.eTest.Admin.util.ColumnNullPropertiesHandler;
 import com.tmax.eTest.Common.model.support.Notice;
+import com.tmax.eTest.Push.dto.CategoryPushRequestDTO;
+import com.tmax.eTest.Push.service.PushService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +14,23 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AdminNoticeService {
+public class AdminNoticeService extends PushService {
     private final AdminNoticeRepository adminNoticeRepository;
     private final AdminNoticeRepositorySupport adminNoticeRepositorySupport;
 
     public Notice createNotice(Notice notice) {
         Timestamp currentDateTime = new Timestamp(System.currentTimeMillis());
         notice.setDraft(0);
+        notice.setViews((long) 0);
         notice.setDateAdd(currentDateTime);
-        return adminNoticeRepository.save(notice);
+        adminNoticeRepository.save(notice);
+        categoryPushRequest(CategoryPushRequestDTO.builder()
+                .category("notice")
+                .title("공지사항")
+                .body(notice.getTitle())
+                .build())
+                .block();
+        return notice;
     }
 
     public Notice draftNotice(Notice notice) {
