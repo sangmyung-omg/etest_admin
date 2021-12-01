4ff220b4cf900920ace9c183b1cac88e10504751
정용조 <yongjo_jeong@tmax.co.kr>
2021-12-01 10:51:52 +0900

    [feat] admin push request by userUuid, category push to selected user(s)

--- eTest/src/main/java/com/tmax/eTest/Push/dto/CategoryPushRequestDTO.java
+++ eTest/src/main/java/com/tmax/eTest/Push/dto/CategoryPushRequestDTO.java
@@ -3,9 +3,12 @@ package com.tmax.eTest.Push.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class CategoryPushRequestDTO {
+    private List<String> userUuid;
     private String category;
     private String title;
     private String body;
