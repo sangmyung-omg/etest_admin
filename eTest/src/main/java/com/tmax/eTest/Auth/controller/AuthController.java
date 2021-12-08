a850ac43af686999fe7ec98300b82c3a0a48d4e5
minjoon_lee <minjoon1995@naver.com>
2021-12-08 14:25:18 +0900

    fix : secure coding

--- eTest/src/main/java/com/tmax/eTest/Auth/controller/AuthController.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/controller/AuthController.java
@@ -1,27 +1,23 @@
 package com.tmax.eTest.Auth.controller;
 
 import com.tmax.eTest.Auth.dto.*;
-
 import com.tmax.eTest.Auth.jwt.JwtTokenUtil;
 import com.tmax.eTest.Auth.repository.UserRepository;
 import com.tmax.eTest.Auth.service.AuthService;
-import com.tmax.eTest.Common.model.user.UserMaster;
-import com.tmax.eTest.LRS.dto.StatementDTO;
 import com.tmax.eTest.LRS.util.LRSAPIManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,47 +39,48 @@ public class AuthController {
         if (ip == null) ip = req.getRemoteAddr();
         return ip;
     }
+
     @PostMapping("/login")
     public CMRespDto<?> jwtCreate(@RequestBody Map<String, Object> data, HttpServletRequest httpServletRequest) {
         String ip = getClientIp(httpServletRequest);
-        return authService.login((String) data.get("providerId"), AuthProvider.valueOf((String) data.get("provider")), ip );
+        return authService.login((String) data.get("providerId"), AuthProvider.valueOf((String) data.get("provider")), ip);
     }
 
     @PostMapping("/signup")
-    public CMRespDto<?> signup(@RequestBody SignUpRequestDto signUpRequestDto){
+    public CMRespDto<?> signup(@RequestBody SignUpRequestDto signUpRequestDto) {
         return authService.singUp(signUpRequestDto);
     }
 
     @PostMapping("/emailDuplicateCheck")
     public CMRespDto<?> duplicateCheck(@RequestBody DuplicateCheckDto duplicateCheckDto) {
-        Map<String,String> data = new HashMap<>();
+        Map<String, String> data = new HashMap<>();
         Boolean res = false;
-        if ( authService.emailDuplicateCheck(duplicateCheckDto.getEmail()) ){
+        if (authService.emailDuplicateCheck(duplicateCheckDto.getEmail())) {
             data.put("email", "중복된 회원이 존재합니다");
             res = true;
-        }else{
-            data.put("email","중복된 회원 없음");
+        } else {
+            data.put("email", "중복된 회원 없음");
         }
         if (res == true) {
             return new CMRespDto<>(201, "중복 회원 존재", data);
-        } else{
+        } else {
             return new CMRespDto<>(200, "회원가입 가능", data);
         }
     }
 
     @PostMapping("/nicknameDuplicateCheck")
     public CMRespDto<?> nicknameDuplicateCheck(@RequestBody DuplicateCheckDto duplicateCheckDto) {
-        Map<String,String> data = new HashMap<>();
+        Map<String, String> data = new HashMap<>();
         Boolean res = false;
-        if ( authService.nickNameDuplicateCheck(duplicateCheckDto.getNickname()) ){
-            data.put("nickname","중복된 회원이 존재합니다");
+        if (authService.nickNameDuplicateCheck(duplicateCheckDto.getNickname())) {
+            data.put("nickname", "중복된 회원이 존재합니다");
             res = true;
-        }else{
-            data.put("nickname","중복된 회원 없음");
+        } else {
+            data.put("nickname", "중복된 회원 없음");
         }
         if (res == true) {
             return new CMRespDto<>(201, "중복 회원 존재", data);
-        } else{
+        } else {
             return new CMRespDto<>(200, "회원가입 가능", data);
         }
     }
@@ -93,9 +90,9 @@ public class AuthController {
     public CMRespDto<?> deleteUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         String result = authService.deleteUser(principalDetails);
         if (result == "True") {
-            return new CMRespDto<>(200,"성공",result);
+            return new CMRespDto<>(200, "성공", result);
         }
-        return new CMRespDto<>(400,"실패",result);
+        return new CMRespDto<>(400, "실패", result);
     }
 
     @GetMapping("/user/logout")
