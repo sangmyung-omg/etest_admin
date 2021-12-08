80e3037764b520503e78fb6e8be723f3a7c521b4
minjoon_lee <minjoon1995@naver.com>
2021-12-08 14:25:18 +0900

    fix : secure coding

--- eTest/src/main/java/com/tmax/eTest/Auth/controller/RefreshController.java
+++ eTest/src/main/java/com/tmax/eTest/Auth/controller/RefreshController.java
@@ -29,57 +29,49 @@ public class RefreshController {
     @Autowired
     JwtTokenUtil jwtTokenUtil;
 
-    @PostMapping(path="/newuser/refresh")
+    @PostMapping(path = "/newuser/refresh")
     public Map<String, Object> requestForNewAccessToken(@RequestBody Map<String, String> m) {
         String accessToken = null;
         String refreshToken = null;
         String refreshTokenFromDb = null;
         String email = null;
         Map<String, Object> map = new HashMap<>();
-
+        accessToken = m.get("accessToken");
+        refreshToken = m.get("refreshToken");
         try {
-            accessToken = m.get("accessToken");
-            refreshToken = m.get("refreshToken");
-            try {
-                email = jwtTokenUtil.getEmailFromToken(accessToken);
-
-            } catch (IllegalArgumentException e) {
-
-
-            }  catch (ExpiredJwtException e) { //expire됐을 때
-                email = e.getClaims().getSubject();
-                log.info("email from expired access token: " + email);
-            }  catch (NoSuchElementException e) {
-                log.info("email이 존재하지 않습니다");
-            }
-            if (refreshToken != null) { //refresh를 같이 보냈으면.
-                try {
-                    Optional<UserMaster> userMasterOptional = userRepository.findByEmail(email);
-                    UserMaster userMaster = userMasterOptional.get();
-                    refreshTokenFromDb = userMasterOptional.get().getRefreshToken();
-                    log.info("rtfrom db: " + refreshTokenFromDb);
-                } catch (IllegalArgumentException e) {
-                    log.warn("illegal argument!!");
-                }
-                //둘이 일치하고 만료도 안됐으면 재발급 해주기.
-                if (refreshToken.equals(refreshTokenFromDb) && !jwtTokenUtil.isTokenExpired(refreshToken)) {
-                    Optional<UserMaster> userMasterOptional  = userRepository.findByEmail(email);
-                    UserMaster userMaster = userMasterOptional.get();
-                    PrincipalDetails principal = PrincipalDetails.create(userMaster);
-                    String newToken =  jwtTokenUtil.generateAccessToken(principal);
-                    map.put("success", true);
-                    map.put("accessToken", newToken);
-                } else {
-                    map.put("success", false);
-                    map.put("msg", "refresh token is expired.");
-                }
-            } else { //refresh token이 없으면
-                map.put("success", false);
-                map.put("msg", "your refresh token does not exist.");
-            }
-        } catch (Exception e) {
-            throw e;
+            email = jwtTokenUtil.getEmailFromToken(accessToken);
+        } catch (ExpiredJwtException e) { //expire됐을 때
+            email = e.getClaims().getSubject();
+            log.info("email from expired access token: " + email);
+        } catch (NoSuchElementException e) {
+            log.info("email이 존재하지 않습니다");
         }
+        if (refreshToken != null) { //refresh를 같이 보냈으면.
+            try {
+                Optional<UserMaster> userMasterOptional = userRepository.findByEmail(email);
+                UserMaster userMaster = userMasterOptional.get();
+                refreshTokenFromDb = userMasterOptional.get().getRefreshToken();
+                log.info("rtfrom db: " + refreshTokenFromDb);
+            } catch (IllegalArgumentException e) {
+                log.warn("illegal argument!!");
+            }
+            //둘이 일치하고 만료도 안됐으면 재발급 해주기.
+            if (refreshToken.equals(refreshTokenFromDb) && !jwtTokenUtil.isTokenExpired(refreshToken)) {
+                Optional<UserMaster> userMasterOptional = userRepository.findByEmail(email);
+                UserMaster userMaster = userMasterOptional.get();
+                PrincipalDetails principal = PrincipalDetails.create(userMaster);
+                String newToken = jwtTokenUtil.generateAccessToken(principal);
+                map.put("success", true);
+                map.put("accessToken", newToken);
+            } else {
+                map.put("success", false);
+                map.put("msg", "refresh token is expired.");
+            }
+        } else { //refresh token이 없으면
+            map.put("success", false);
+            map.put("msg", "your refresh token does not exist.");
+        }
+
         log.info("m: " + m);
         return map;
     }
