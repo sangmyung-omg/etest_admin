95ff277026fe46b24f03b28a8c4aa8e6c05503c2
minjoon_lee <minjoon1995@naver.com>
2021-12-17 18:46:39 +0900

    feature : 1-1 inquiry attachment get method 추가

--- eTest/src/main/java/com/tmax/eTest/Admin/CustomerSupport/controller/InquiryController.java
+++ eTest/src/main/java/com/tmax/eTest/Admin/CustomerSupport/controller/InquiryController.java
@@ -7,18 +7,31 @@ import com.tmax.eTest.Admin.CustomerSupport.model.dto.InquiryDTO;
 import com.tmax.eTest.Admin.CustomerSupport.service.InquiryService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController("CustomerSupportInquiryController")
 @RequestMapping(value="submaster/customerSupport", produces = MediaType.APPLICATION_JSON_VALUE)
 public class InquiryController {
     protected final Log LOGGER = LogFactory.getLog(getClass());
+
+    @Value("${file.path}")
+    private String path;
 
     @Autowired
     InquiryService inquiryService;
@@ -54,7 +67,22 @@ public class InquiryController {
         return ResponseEntity.ok().body("success delete inquiry");
     }
 
+    @GetMapping(value="/inquiry/attachment")
+    public ResponseEntity<Resource> attachment(@Param("filename") String filename){
+        String temp = path+"/inquiry/";
+        Path filePath = null;
+        filePath = Paths.get(temp+filename);
+        HttpHeaders header = new HttpHeaders();
+        Tika tika = new Tika();
+        String mimeType;
+        try {
+            mimeType = tika.detect(filePath);
+            header.add("Content-Type", mimeType);
+        } catch (IOException e) {
+            LOGGER.debug("mime type Error");
+        }
 
-
-
+        Resource resource = new FileSystemResource(filePath);
+        return new ResponseEntity<Resource>(resource,header, HttpStatus.OK);
+    }
 }
