b74a3626d60ca1382344776fb057b896c001c743
youngguen_song <younggeun_song@tmax.co.kr>
2022-01-20 15:17:22 +0900

    [fix] modify get thumbnail from naver post

--- eTest/src/main/java/com/tmax/eTest/Contents/service/ImageService.java
+++ eTest/src/main/java/com/tmax/eTest/Contents/service/ImageService.java
@@ -2,11 +2,14 @@ package com.tmax.eTest.Contents.service;
 
 import com.tmax.eTest.Contents.exception.ContentsException;
 import com.tmax.eTest.Contents.exception.ErrorCode;
+import com.tmax.eTest.Contents.util.CommonUtils;
 
 import org.jsoup.Connection;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,15 +18,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ImageService {
 
+  @Autowired
+  private CommonUtils commonUtils;
+
   public String getThumbnail(String url) {
+    log.info(url);
 
     String thumbnail = "";
     Connection conn = Jsoup.connect(url)
-        .userAgent("Mozilla");
-    // .header("User-Agent", "Mozilla/5.0");
+        .userAgent("Mozilla")
+        .header("User-Agent", "Mozilla/5.0")
+        .timeout(5000);
     try {
       Document document = conn.get();
-      thumbnail = document.select("meta[property=og:image]").first().attr("content");
+      log.info("Get Succeded!");
+
+      Element element = document.selectFirst("meta[property=og:image]");
+      if (commonUtils.objectNullcheck(element))
+        element = document.selectFirst("meta[property=og:secure_image]");
+      thumbnail = element.attr("content");
+
       log.info("Thumbnail URL = " + thumbnail);
     } catch (HttpStatusException e) {
       log.error("Request URL Error: " + e.getMessage());
