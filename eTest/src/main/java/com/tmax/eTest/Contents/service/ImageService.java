b60653378256b05f2a30f4ed8e16d5b71f48e088
youngguen_song <younggeun_song@tmax.co.kr>
2022-01-28 13:13:15 +0900

    [fix] modify to get thumbnail api

--- eTest/src/main/java/com/tmax/eTest/Contents/service/ImageService.java
+++ eTest/src/main/java/com/tmax/eTest/Contents/service/ImageService.java
@@ -7,6 +7,7 @@ import com.tmax.eTest.Contents.util.CommonUtils;
 import org.jsoup.Connection;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,10 @@ public class ImageService {
 
     String thumbnail = "";
     Connection conn = Jsoup.connect(url)
-        .userAgent("Mozilla")
-        .header("User-Agent", "Mozilla/5.0")
+        // .userAgent("Mozilla")
+        // .header("User-Agent", "Mozilla/5.0")
+        .userAgent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36")
         .timeout(5000);
     try {
       Document document = conn.get();
@@ -39,7 +42,8 @@ public class ImageService {
       thumbnail = element.attr("content");
 
       log.info("Thumbnail URL = " + thumbnail);
-    } catch (HttpStatusException e) {
+    } catch (java.net.MalformedURLException | HttpStatusException | UnsupportedMimeTypeException
+        | java.net.SocketTimeoutException e) {
       log.error("Request URL Error: " + e.getMessage());
       throw new ContentsException(ErrorCode.OPENGRAPH_ERROR, "Request URL = " + url);
     } catch (Exception e) {
