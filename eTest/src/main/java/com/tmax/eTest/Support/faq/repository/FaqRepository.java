b5872299033309df300541f5168a22a949d86167
sangmyung-omg <64184699+sangmyung-omg@users.noreply.github.com>
2022-10-14 17:21:53 +0900

    Delete eTest/src/main/java/com/tmax/eTest/Support directory

--- eTest/src/main/java/com/tmax/eTest/Support/faq/repository/FaqRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tmax.eTest.Support.faq.repository;
-
-import com.tmax.eTest.Common.model.support.FAQ;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FaqRepository extends JpaRepository<FAQ, Long> {}
--- eTest/src/main/java/com/tmax/eTest/Support/notice/repository/NoticeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tmax.eTest.Support.notice.repository;
-
-import com.tmax.eTest.Common.model.support.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoticeRepository extends JpaRepository<Notice, Long> {}
