ab736d8bc4d7d3e69bbd6be7cd484e5f78f628fd
sangmyung-omg <64184699+sangmyung-omg@users.noreply.github.com>
2022-10-14 17:26:08 +0900

    Delete eTest/src/main/java/com/tmax/eTest/Common/repository/user directory

--- eTest/src/main/java/com/tmax/eTest/Common/repository/user/UserEmbeddingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tmax.eTest.Common.repository.user;
-
-import com.tmax.eTest.Common.model.user.UserEmbedding;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserEmbeddingRepo extends JpaRepository<UserEmbedding, String>{
-    
-}
--- eTest/src/main/java/com/tmax/eTest/Common/repository/user/UserMasterRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tmax.eTest.Common.repository.user;
-
-import com.tmax.eTest.Common.model.user.UserMaster;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserMasterRepo extends JpaRepository<UserMaster, String>{
-    
-}
