24c6294ca60bf401a8bbb991668858c1ac8562d7
sangmyung-omg <64184699+sangmyung-omg@users.noreply.github.com>
2022-10-14 17:20:58 +0900

    Delete eTest/src/main/java/com/tmax/eTest/Admin directory

--- eTest/src/main/java/com/tmax/eTest/Admin/ManageUser/repository/ReasonForDownloadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tmax.eTest.Admin.ManageUser.repository;//package com.tmax.eTest.ManageUser.repository;
-//
-//import com.tmax.eTest.ManageUser.model.ReasonForDownload;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ReasonForDownloadRepository extends JpaRepository<ReasonForDownload, Long> {
-//}
